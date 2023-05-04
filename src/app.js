aae6afcf807820466e8b60350afd6ce3b4999006
HeliaHosseini2 <hosseini.helia93@gmail.com>
2023-05-04 20:06:29 +0330

    added js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,25 @@
+function formatDate(timestamp){
+    // calculate the date
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    let date = new Date(timestamp)
+    let hours = date.getHours()
+    if (hours < 10) {return `0${hours}`}
+    let minutes = date.getMinutes()
+    if (minutes < 10) {return `0${minutes}`}
+    let day =  days[date.getDay()] 
+
+    return (`${day}  ${hours}:${minutes}`)
+
+
+}
+
 function displayTemperature(response){console.log(response.data)
 document.querySelector("#temperature").innerHTML = Math.round(response.data.temperature.current)
 document.querySelector("#city").innerHTML= response.data.city
 document.querySelector("#description").innerHTML = response.data.condition.description 
 document.querySelector("#humidity").innerHTML = response.data.temperature.humidity
-document.querySelector("#wind").innerHTML =  Math.round(response.data.wind.speed) }
+document.querySelector("#wind").innerHTML =  Math.round(response.data.wind.speed)
+document.querySelector("#date").innerHTML = formatDate(response.data.time) }
 
 
 let apiKey ="d921b380ob8fd44ea6tdafd048cb353a"
